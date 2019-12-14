7262e0d8443ee4219df589b0eac124cb8b3e730b
yeeeshiuan <yeeeshiuan.huang@gmail.com>
2019-12-14 18:48:37 +0800

    add back-end SendAllMessage method

--- SignalRServices/Hubs/MessageHub.cs
+++ SignalRServices/Hubs/MessageHub.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SignalRServices.Hubs
 {
     public class MessageHub : Hub
     {
         public static int prevNumber { get; set; }
+
+        public static Dictionary<string, int> prevData = new Dictionary<string, int> ();
 
         public async Task SendMessage(string user, string message)
         {
@@ -31,6 +35,41 @@ namespace SignalRServices.Hubs
                     }
                 }
                 prevNumber = numVal;
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public async Task SendAllMessage(string user, string message)
+        {
+            try
+            {
+                int numVal = int.Parse(message);
+
+                int pastNumber;
+                if (!prevData.ContainsKey(user))
+                {
+                    pastNumber = 0;
+                    prevData.Add(user, pastNumber);
+                } else {
+                    pastNumber = prevData[user];
+                }
+
+                int diff = numVal - pastNumber;
+                if (Math.Abs(diff) > 4)
+                {
+                    prevData[user] = numVal;
+                    if (Math.Sign(diff) > 0)
+                    {
+                        await Clients.All.SendAsync("ReceiveAllMessage", user, JsonConvert.SerializeObject(prevData));
+                    }
+                    else if (Math.Sign(diff) < 0)
+                    {
+                        await Clients.All.SendAsync("ReceiveAllMessage", user, JsonConvert.SerializeObject(prevData));
+                    }
+                }
             }
             catch (FormatException e)
             {
--- SignalRServices/wwwroot/js/message.js
+++ SignalRServices/wwwroot/js/message.js
@@ -13,6 +13,14 @@ connection.on("ReceiveMessage", function (user, message) {
     document.getElementById("messagesList").appendChild(li);
 });
 
+// connection.on("ReceiveAllMessage", function (user, message) {
+//     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//     var encodedMsg = user + " says " + msg;
+//     var li = document.createElement("li");
+//     li.textContent = encodedMsg;
+//     document.getElementById("messagesList").appendChild(li);
+// });
+
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
@@ -28,5 +36,8 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     connection.invoke("SendActivityMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
+    // connection.invoke("SendAllMessage", user, message).catch(function (err) {
+    //     return console.error(err.toString());
+    // });
     event.preventDefault();
 });
