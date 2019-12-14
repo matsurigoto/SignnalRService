1db97159cbaf9e5f06a85893838fcaaeea8a0e16
Duran Hsieh <chhsie@microsoft.com>
2019-12-14 21:32:38 +0800

    update

--- SignalRServices/Hubs/MessageHub.cs
+++ SignalRServices/Hubs/MessageHub.cs
@@ -11,30 +11,47 @@ namespace SignalRServices.Hubs
         public static int prevNumber { get; set; }
 
         public static Dictionary<string, int> prevData = new Dictionary<string, int>();
+        public static Dictionary<string, int> countData = new Dictionary<string, int>();
 
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        public async Task SendActivityMessage(string user, string message)
+        public async Task GetInitMessage()
+        {
+            if (countData.Count != 0)
+            {
+                await Clients.All.SendAsync("ReceiveInitMessage", JsonConvert.SerializeObject(countData));
+            }    
+        }
+
+        public async Task SendAllMessage(string user, string message)
         {
             try
             {
+                user = "user" + user;
                 int numVal = int.Parse(message);
-                int diff = numVal - prevNumber;
-                if (Math.Abs(diff) > 4)
+
+                int pastNumber = 0;
+                if (!prevData.ContainsKey(user))
                 {
-                    if (Math.Sign(diff) > 0)
-                    {
-                        await Clients.All.SendAsync("ReceiveActivityMessage", user, message);
-                    }
-                    else if (Math.Sign(diff) < 0)
-                    {
-                        await Clients.All.SendAsync("ReceiveActivityMessage", user, message);
-                    }
+                    prevData.Add(user, pastNumber);
+                    countData.Add(user, 0);
+                } 
+                else
+                {
+                    pastNumber = prevData[user];
                 }
-                prevNumber = numVal;
+
+                int diff = numVal - pastNumber;
+                prevData[user] = numVal;
+
+                if (Math.Abs(diff) > 3)
+                {
+                    countData[user] += 1;
+                    await Clients.All.SendAsync("ReceiveAllMessage", user, JsonConvert.SerializeObject(countData));
+                }
             }
             catch (FormatException e)
             {
@@ -42,39 +59,12 @@ namespace SignalRServices.Hubs
             }
         }
 
-        public async Task SendAllMessage(string user, string message)
+        public async Task RemoveUser(string user)
         {
-            try
-            {
-                int numVal = int.Parse(message);
-
-                int pastNumber;
-                if (!prevData.ContainsKey(user))
-                {
-                    pastNumber = 0;
-                    prevData.Add(user, pastNumber);
-                } else {
-                    pastNumber = prevData[user];
-                }
-
-                int diff = numVal - pastNumber;
-                if (Math.Abs(diff) > 4)
-                {
-                    prevData[user] = numVal;
-                    if (Math.Sign(diff) > 0)
-                    {
-                        await Clients.All.SendAsync("ReceiveAllMessage", user, JsonConvert.SerializeObject(prevData));
-                    }
-                    else if (Math.Sign(diff) < 0)
-                    {
-                        await Clients.All.SendAsync("ReceiveAllMessage", user, JsonConvert.SerializeObject(prevData));
-                    }
-                }
-            }
-            catch (FormatException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            user = "user" + user;
+            prevData.Remove(user);
+            countData.Remove(user);
+            await Clients.All.SendAsync("ReceiveAllMessage", user, JsonConvert.SerializeObject(countData));
         }
     }
 }
--- SignalRServices/wwwroot/js/activity.js
+++ SignalRServices/wwwroot/js/activity.js
@@ -17,27 +17,52 @@ connection.on("ReceiveAllMessage", function (user, message) {
             userDom.id = key;
             // TODO: create Card html....
             userDom.innerHTML = `
-                <p class="player">${key}</p>
+                <p class="player">${key.replace('user','')}</p>
                 <p>次數:<span class="count">0</span></p>
-                <div><img id="image1" src="/walk.gif" /></div>
+                <div><img id="image${key}" src="/walk.gif" /></div>
+            `;
+            containerHtml.appendChild(userDom);
+        }
+
+        userDom.querySelector('.count').innerText = person[key];
+        
+    });
+
+    var userDom = containerHtml.querySelector(`#${user}`);
+    userDom.querySelector(`#image${user}`).src = "/run.gif";
+    window.setTimeout((() => document.getElementById(`image${user}`).src = "/walk.gif"), 10000);
+});
+
+
+connection.on("ReceiveInitMessage", function (message) {
+    console.log(message);
+    var person = JSON.parse(message);
+    var containerHtml = document.querySelector('.player-result');
+    Object.keys(person).forEach(key => {
+        console.log(containerHtml);
+        var userDom = containerHtml.querySelector(`#${key}`);
+        console.log(userDom);
+        if (!userDom) {
+            userDom = document.createElement("div");
+            userDom.className = "player-card";
+            userDom.id = key;
+            // TODO: create Card html....
+            userDom.innerHTML = `
+                <p class="player">${key.replace('user', '')}</p>
+                <p>次數:<span class="count">0</span></p>
+                <div><img id="image${key}" src="/walk.gif" /></div>
             `;
             containerHtml.appendChild(userDom);
         }
 
         userDom.querySelector('.count').innerText = person[key];
     });
-    //for (var key in person)
-    //{
-    //    console.log(key);
-    //}
-    
-    //document.getElementById("player1").innerText = parseInt(document.getElementById("player1").innerText) + 1;
-    //document.getElementById("image1").src = "/run.gif";
-    //window.setTimeout((() => document.getElementById("image1").src = "/walk.gif"), 10000);
 });
 
 connection.start().then(function () {
-    
+    connection.invoke("GetInitMessage").catch(function (err) {
+        return console.error(err.toString());
+    });
 }).catch(function (err) {
     return console.error(err.toString());
 });
