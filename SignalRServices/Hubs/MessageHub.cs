e787f1a71a8a07b3fef349b9869e7b7c06f9909c
Duran Hsieh <chhsie@microsoft.com>
2019-12-14 16:26:49 +0800

    update

--- SignalRServices/Hubs/MessageHub.cs
+++ SignalRServices/Hubs/MessageHub.cs
@@ -1,13 +1,35 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
 
 namespace SignalRServices.Hubs
 {
     public class MessageHub : Hub
     {
+        public int prevNumber { get; set; }
+
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            try
+            {
+                int numVal = int.Parse(message);
+                int diff = numVal - prevNumber;
+                if (Math.Abs(diff) > 4)
+                {
+                    if(Math.Sign(diff) > 0)
+                    {
+                        await Clients.All.SendAsync("ReceiveMessage", user, message);
+                    }
+                    else if (Math.Sign(diff) < 0)
+                    {
+                        await Clients.All.SendAsync("ReceiveMessage", user, message);
+                    }
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
